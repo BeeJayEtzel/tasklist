3f3519f5620f5426aad502bf3cc2f7f4e860fb1e
Brian Etzel <brianetzel@gmail.com>
2018-03-22 22:33:52 -0400

    Create initial homepage HTML

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+
+@section('title')
+    Tasklist
+@endsection
+
+@section('content')
+    <h1>Tasklist</h1>
+    <p>Create tasks. Define subtasks. Take notes.</p>
+    <p><span class="bold">Get work done.</span></p>
+    <br>
+    <a href="/register" class="button">Create account</a>
+@endsection
