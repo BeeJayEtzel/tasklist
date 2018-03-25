61a662383a41096dc638d583f2dfa53b6ed4564f
Brian Etzel <brianetzel@gmail.com>
2018-03-24 22:33:57 -0400

    Create home controller

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use \App\User;
+
+class HomeController extends Controller
+{
+    /**
+     * Shows the homepage for a given user
+     */
+    public function show()
+    {
+        return null;
+    }
+
+
+}
