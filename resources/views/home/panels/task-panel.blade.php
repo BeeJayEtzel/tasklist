1a3d63dfb96c1f95cbdbdf4900c3ad1f190f35a1
Brian Etzel <brianetzel@gmail.com>
2018-03-27 15:11:50 -0400

    Add conditional rendering to task-panel div

--- resources/views/home/panels/task-panel.blade.php
+++ resources/views/home/panels/task-panel.blade.php
@@ -1,11 +1,13 @@
-<div class="task-panel" id='task-panel'>
+<div class="task-panel" id='task-panel' v-if="tasks != 0">
+    <h2>Project name</h2>
+        <div class="task">
+            <p v-for="task in tasks" :id='task.id' @click="getSubtasks()">@{{ task.description }}</p>
+        </div>
+
     <form action="/task/create" method="post" @submit.prevent="addTask()">
         <input type="text" name="description" v-model="description">
         <input type="datetime-local" name="due_date" v-model="due_date">
         <input type="submit" value="Submit">
     </form>
-    <h2>Project name</h2>
-        <div class="task">
-            <p v-for="task in tasks" @click="getSubtasks(this.taskId)">@{{ task.description }}</p>
-        </div>
+
 </div>
