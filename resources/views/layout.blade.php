a9df5ccc64065009da316c0de420112adbb24924
Brian Etzel <brianetzel@gmail.com>
2018-04-17 10:23:54 -0400

    Add script for FA cdn

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -17,5 +17,6 @@
 <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 <script src="/js/app.js"></script>
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+<script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>
 </body>
 </html>
