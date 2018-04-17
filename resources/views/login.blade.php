59891402f0cb734a1a20212b0164a455d98411f3
Brian Etzel <brianetzel@gmail.com>
2018-04-16 20:43:13 -0400

    Styling for forms on login/register

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,21 +1,23 @@
-<div class="form">
+@extends('layout')
+
+<div class="login">
     <form action="/login" method="post">
         {{ csrf_field() }}
         <label for="username">Username</label>
         <input type="text" name="username" required>
         <label for="password">Password</label>
         <input type="password" name="password" required>
-        <input type="submit" value="Log In">
+        @if (count($errors))
+        <div class="form-errors">
+                @foreach($errors->all() as $error)
+                <p class="error">{{ $error }}</p>
+                @endforeach
+        </div>
+        @endif
+        <div class="form-footer">
+            <input type="submit" value="Log In">
+        </div>
     </form>
 </div>
 
-@if (count($errors))
-<div class="form-errors">
-    <ul>
-    @foreach ($errors->all() as $error)
-        <li>{{ $error }}</li>
-    @endforeach
-    </ul>
-</div>
-@endif
     
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,4 +1,7 @@
-<div class="form">
+@extends("layout")
+
+<div class="register">
+    <h1 class="form-header">Create an Account</h1>
     <form action="/register" method="post">
         {{ csrf_field() }}
         <label for="username">Username</label>
@@ -9,14 +12,15 @@
         <input type="password" name="password" required>
         <label for="password_confirmation">Confirm Password</label>
         <input type="password" name="password_confirmation" required>
-        <input type="submit" value="Register">
+        <div class="form-footer">
+            <input type="submit" value="Create Account">
+            <p>Already have an account? <a href="/login">Log In.</a></p>
+        </div>
     </form>
     @if (count($errors))
         <div class="form-errors">
-            <ul>
                 @foreach($errors->all() as $error)
-                <li>{{ $error }}</li>
-            </ul>
+                <p class="error">{{ $error }}</p>
                 @endforeach
         </div>
     @endif
